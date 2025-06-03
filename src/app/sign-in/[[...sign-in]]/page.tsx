cecbf9bfd50d908a48cc25a13f8e513e97762d25
Akil Khatri <khatriakil104@gmail.com>
2025-06-03 18:50:41 +0530

    Setup Signup and Signin pages with Clerk

---
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignIn } from "@clerk/nextjs";
+
+export default function SignInPage() {
+    return (
+        <div className="flex items-center justify-center w-screen min-h-screen bg-gradient-to-r from-gray-700 via-gray-900 to-black text-white">
+            <SignIn />
+        </div>
+    )
+}
